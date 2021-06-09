6dbf043a58a5ae8edc53aece9d841aab3ad98ef3
Debnath <souvik.debnath@refinitiv.com>
2021-06-09 21:02:45 +0530

    first commit

---
+++ src/context/Context.js
@@ -0,0 +1,4 @@
+//TODO: Create context: ContactContext
+import {createContext} from "react";
+
+export const ContactContext = createContext();
---
+++ src/utils/config.js
@@ -0,0 +1,17 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyDip7h9ijUtUgJDra58xMQEsOM98rlaP1U",
+  authDomain: "mygitapp-d7d6c.firebaseapp.com",
+  databaseURL: "https://mygitapp-d7d6c-default-rtdb.firebaseio.com",
+  projectId: "mygitapp-d7d6c",
+  storageBucket: "mygitapp-d7d6c.appspot.com",
+  messagingSenderId: "549439618277",
+  appId: "1:549439618277:web:41205beb2da342f5cc280c"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
